4d524dc17563829289914e0f9b0b55ba3bfe91bf
Michael Meyer <michael.meyer@improving.com>
2021-05-24 15:44:54 -0500

    Initial Commit

---
+++ AgentsCustomersOrders/ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AgentsCustomersOrders.Models;
+
+namespace AgentsCustomersOrders.ViewModels.Home
+{
+    public class HomeViewModel
+    {
+        public List<Agent> Agents { get; set; }
+    }
+}
